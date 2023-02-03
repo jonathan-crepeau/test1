c33bfcfd5d4c12ddbde076174e8479259db58b6c
jonathan-crepeau <jon.crepeau@gmail.com>
2023-02-02 17:05:43 -0800

    updated for/in loop

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-console.log('app.js reporting for duty..');
+// console.log('app.js reporting for duty..');
 
-// SECTION - Bindings
-const btn = document.querySelector('input');
-
-btn.addEventListener('click', printHello);
-
-function printHello(event) {
-    event.preventDefault();
-    console.log('Why hello there!');
+const indexedObj = {
+    0: {key: 'type', value: 'dog'},
+    1: {key: 'color', value: 'black'},
+    2: {key: 'gender', value: 'female'}
 }
+
+const array1 = ['a', 'b', 'c'];
+
+let string = '';
+for (index in indexedObj) {
+    string = string + `?${indexedObj[index].key}=${indexedObj[index].value}`;  
+}
+console.log(string)
+
+
+
+
